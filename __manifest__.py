f7cc502b9871063f8ba25e8f1feb81e1a3668215
Mauricio Rubio Haro <marh@odoo.com>
2023-07-18 11:18:12 -0600

    gitignore file

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Odoo Academy',
+    'summary' : 'Module to handel Course and Sessions',
+    'description' : """Module to handle:
+    - Courses
+    - Sessions
+    - Attendees
+    """,
+    'license' : 'OPL-1',
+    'author' : 'Mauricio Rubio',
+    'website' : 'www.odoo.com',
+    'category' : 'Custom Modules / Technical Training',
+    'depends' : ['base'],
+    'data': [],
+    'demo': [],
+    'application' : True,
+
+}   
