83bd7903161012040248ca845a08146d01f5831c
Mauricio Rubio Haro <marh@odoo.com>
2023-07-20 16:03:14 -0600

    training 10

---
+++ models/sale_order.py
@@ -0,0 +1,7 @@
+from odoo import fields, models
+
+# class Course(models.Model):
+#     _inherit = "sale.order"
+#     session_id = fields.Many2one(comodel_name="academy.session", string ="Related Sessions", ondelete="set null")
+#     instructor_id = fields.Many2one(string="Session Instructor", related='session_id.instructor_id')
+#     student_ids = fields.Many2one(string="Student", related='session_id.student_ids ')
